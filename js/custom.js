395d1e72d618372868ec4796a17171ef72ce65c2
langwu <wulang19910216@gmail.com>
2018-01-16 11:43:08 -0500

    asdf

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+(function($) {
+
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".sidenav a").on('click', function(event) {
+    var hash = this.hash;
+    if (hash) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function() {
+        window.location.hash = hash;
+      });
+    }
+
+  });
+
+  $("#slideshow > div:gt(0)").hide();
+
+  setInterval(function() {
+    $('#slideshow > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow');
+  }, 3000);
+
+})(jQuery);
